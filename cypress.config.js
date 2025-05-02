ba41fd5dae3226ec4ad8ddf8a89f8eb01d53f3b7
buraktuna1993 <77328510+buraktuna1993@users.noreply.github.com>
2025-05-02 18:35:50 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -8,10 +8,10 @@ module.exports = defineConfig({
     supportFile: 'cypress/support/index.js'
   },
   pageLoadTimeout: 60000,
-  responseTimeout: 20000,
+  responseTimeout: 40000,
   retries: {
     "runMode": 2,
-    "openMode": 0
+    "openMode": 2
   },
  // blockHosts: "*.widget.trustpilot.com/trustbox-data"
 
